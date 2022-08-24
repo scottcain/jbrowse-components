49ef90f122919db0fa6c7923f2047ccba7ea0c2f
Colin <colin.diesh@gmail.com>
2022-08-24 14:59:52 -0600

    Weird example

--- plugins/sequence/src/SequenceSearchAdapter/SequenceSearchAdapter.ts
+++ plugins/sequence/src/SequenceSearchAdapter/SequenceSearchAdapter.ts
@@ -9,6 +9,7 @@ import {
   Region,
   complement,
   reverse,
+  doesIntersect2,
 } from '@jbrowse/core/util'
 import { toArray } from 'rxjs/operators'
 
@@ -28,67 +29,57 @@ export default class extends BaseFeatureDataAdapter {
 
   public getFeatures(query: Region, opts: BaseOptions) {
     return ObservableCreate<Feature>(async observer => {
-      const sequenceAdapter = await this.configure()
+      const feats = [
+        {
+          uniqueId: '1541597311-match-19554-p',
+          refName: 'ctgA',
+          start: 19554,
+          end: 19560,
+          strand: 1,
+        },
+        {
+          uniqueId: '1541597311-match-20380-p',
+          refName: 'ctgA',
+          start: 20380,
+          end: 20386,
+          strand: 1,
+        },
+        {
+          uniqueId: '1541597311-match-23576-p',
+          refName: 'ctgA',
+          start: 23576,
+          end: 23582,
+          strand: 1,
+        },
+        {
+          uniqueId: '1541597311-match-22032-n',
+          refName: 'ctgA',
+          start: 22032,
+          end: 22038,
+          strand: -1,
+        },
+        {
+          uniqueId: '1541597311-match-22065-n',
+          refName: 'ctgA',
+          start: 22065,
+          end: 22071,
+          strand: -1,
+        },
+      ]
+
       const hw = 1000
       let { start: queryStart, end: queryEnd } = query
       queryStart = Math.max(0, queryStart - hw)
       queryEnd += hw
-
-      if (queryEnd < 0 || queryStart > queryEnd) {
-        observer.complete()
-        return
-      }
-
-      const ret = sequenceAdapter.getFeatures(
-        {
-          ...query,
-          start: queryStart,
-          end: queryEnd,
-        },
-        opts,
-      )
-      const feats = await ret.pipe(toArray()).toPromise()
-      const residues: string = feats[0]?.get('seq') || ''
-      const search = this.getConf('search')
-      const searchFoward = this.getConf('searchForward')
-      const searchReverse = this.getConf('searchReverse')
-
-      if (search) {
-        if (searchFoward) {
-          const matches = residues.matchAll(new RegExp(search, 'g'))
-          for (const match of matches) {
-            const s = queryStart + (match.index || 0)
-            observer.next(
-              new SimpleFeature({
-                uniqueId: `${this.id}-match-${s}-p`,
-                refName: query.refName,
-                start: s,
-                end: s + search.length,
-                strand: 1,
-              }),
-            )
-          }
+      feats.forEach(f => {
+        if (doesIntersect2(queryStart, queryEnd, f.start, f.end)) {
+          console.log(queryStart, queryEnd, JSON.stringify(f), f.start, f.end)
+          observer.next(new SimpleFeature(f))
         }
-        if (searchReverse) {
-          const matches = complement(residues).matchAll(
-            new RegExp(reverse(search), 'g'),
-          )
-          for (const match of matches) {
-            const s = queryStart + (match.index || 0)
-            observer.next(
-              new SimpleFeature({
-                uniqueId: `${this.id}-match-${s}-n`,
-                refName: query.refName,
-                start: s,
-                end: s + search.length,
-                strand: -1,
-              }),
-            )
-          }
-        }
-      }
+      })
 
       observer.complete()
+      console.log('done')
     })
   }
 
